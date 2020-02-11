a60a157b1cd39b6bb9ad1a47a6b8bde532f5bcaf
Jonathan Scialpi <scialpi27@gmail.com>
2020-02-11 19:04:09 +0000

    Altering the test to have 1 node over sees and 1 node on shore

--- workflows/src/main/kotlin/com/template/flows/SendAttachmentCaller.kt
+++ workflows/src/main/kotlin/com/template/flows/SendAttachmentCaller.kt
@@ -26,10 +26,12 @@ class SendAttachmentCaller(
 
     @Suspendable
     override fun call(){
+
         //grab the list of hashes for the generated files
         val queryCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
         val hashFilesState = serviceHub.vaultService.queryBy<HashedFilesState>(queryCriteria).states.single()
         val fileSet = hashFilesState.state.data.fileHashes
+
         //for each of the hashes found, call the send attachment flow for the buyer and seller
         if (recipientB != null) {
             for(attachmentHash in fileSet){
