89ab485ee79a5f44e7423137900b6b038eb7e8db
Jonathan Scialpi <scialpi27@gmail.com>
2020-02-19 11:02:09 -0500

    Update generate-test-files.py

--- Python/generate-test-files.py
+++ Python/generate-test-files.py
@@ -7,16 +7,16 @@ import requests
 #@PARAM: content byte is the dummy char that will be repeated in the file to reach the file_size.
 #@PARAM: file_size int how big each file should be in megabytes.
 #@PARAM: number_of_files int the quantity of the files to generate on the node.
-def generate_hashed_files(url, test_name, content, file_size, number_of_files):
-    for i in range(10):         
+def generate_hashed_files(url, test_name, content, file_size, number_of_files, loops):
+    for i in range(loops):         
         response = requests.post(url, json={
             "testName": test_name + str(i) + "-",
-            "content": 0,
-            "fileSize": 10,
-            "numberOfFiles": 10
+            "content": content,
+            "fileSize": file_size,
+            "numberOfFiles": number_of_files
         })
 
         print(json.loads(response.text)["tx"]["outputs"][0]["data"]["fileHashes"])
 
 url = "http://localhost:8999/api/rest/cordapps/workflows/flows/com.template.flows.HashedFilesIssue"
-generate_hashed_files(url, "useast-100-10mb-01-", 0, 10, 10)
+generate_hashed_files(url, "useast-100-10mb-01-", 0, 10, 10, 10)
