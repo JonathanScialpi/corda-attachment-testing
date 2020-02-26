4c62cb418bbea44dfe8f7e528724fe9b61f6bd11
Jonathan Scialpi <scialpi27@gmail.com>
2020-02-25 21:15:54 -0500

    created the spring generate files endpoint

--- CorDapps/clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ CorDapps/clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -1,9 +1,20 @@
 package com.template.webserver
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.template.flows.HashedFilesIssue
+import com.template.states.GeneratedFilesState
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.messaging.startFlow
+import net.corda.core.messaging.startTrackedFlow
+import net.corda.core.utilities.getOrThrow
+import org.apache.catalina.servlet4preview.http.HttpServletRequest
+import org.apache.logging.log4j.core.appender.rolling.FileSize
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.nio.file.Files
 
 /**
  * Define your API endpoints here.
@@ -18,8 +29,30 @@ class Controller(rpc: NodeRPCConnection) {
 
     private val proxy = rpc.proxy
 
-    @GetMapping(value = ["/templateendpoint"], produces = ["text/plain"])
-    private fun templateendpoint(): String {
-        return "Define an endpoint here."
+    @GetMapping(value = ["/status"])
+    private fun isAlive() = "Up and running!"
+
+    @RequestMapping(value = "/generateAttachments", method = arrayOf(RequestMethod.POST))
+    private fun hashedFilesIssue(@RequestBody generatedFiles: NewFilesRequest): ResponseEntity<Any?> {
+        val start = System.currentTimeMillis()
+        var result = proxy.startFlow(
+                ::HashedFilesIssue,
+                generatedFiles.testName,
+                generatedFiles.content.toByte(),
+                generatedFiles.fileSize.toInt(),
+                generatedFiles.numberOfFiles.toInt())
+        val elapsedTime = System.currentTimeMillis() - start
+        val hashedFilesList = (result.returnValue.get().tx.outputs[0].data as GeneratedFilesState).fileHashes.toString()
+        val responseMap = HashMap<String, Any>()
+        responseMap["HashedFiles"] = hashedFilesList
+        responseMap["ElapsedTime"] = elapsedTime
+        return ResponseEntity.ok(responseMap.toString())
     }
+
+    data class NewFilesRequest @JsonCreator constructor(
+            val testName: String,
+            val content: String,
+            val fileSize: String,
+            val numberOfFiles: String
+    )
 }
